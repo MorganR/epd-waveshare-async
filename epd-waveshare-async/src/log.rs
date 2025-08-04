a3457f7b7ded4bfe12b0fd88c876da0f098b5da6
Morgan Roff <morgan@roff.dev>
2025-08-04 22:38:31 +0100

    Fix occasional bug in debug_assert

--- epd-waveshare-async/src/log.rs
+++ epd-waveshare-async/src/log.rs
@@ -31,9 +31,9 @@ macro_rules! warn_log {
 macro_rules! debug_assert {
     ($assertion:expr, $message:expr) => {
         #[cfg(feature = "defmt")]
-        defmt::debug_assert!($assertion, $message,);
+        defmt::debug_assert!($assertion, $message);
         #[cfg(not(feature = "defmt"))]
-        debug_assert!($assertion, $message,);
+        core::debug_assert!($assertion, $message);
     };
 }
 
